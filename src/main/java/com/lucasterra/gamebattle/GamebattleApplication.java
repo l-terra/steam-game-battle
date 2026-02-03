e41d162d2d825396eed3bc0c9225819e05101af2
Lucas Terra <lucas.terra8587@gmail.com>
2026-02-02 22:20:32 -0300

    first commit

---
+++ src/main/java/com/lucasterra/gamebattle/GamebattleApplication.java
@@ -0,0 +1,13 @@
+package com.lucasterra.gamebattle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamebattleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamebattleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasterra/gamebattle/GamebattleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasterra.gamebattle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamebattleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
