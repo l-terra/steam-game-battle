7b62ed068548acc0c8f23eb57b973750679d8de0
Lucas Terra <lucas.terra8587@gmail.com>
2026-02-07 20:37:43 -0300

    test: testing repositories and database connectivity

--- src/main/java/com/lucasterra/gamebattle/SteamSmokeTest.java
+++ src/main/java/com/lucasterra/gamebattle/SteamSmokeTest.java
@@ -1,34 +1,49 @@
 package com.lucasterra.gamebattle;
 
+import com.lucasterra.gamebattle.domain.Game;
+import com.lucasterra.gamebattle.domain.User;
+import com.lucasterra.gamebattle.domain.UserLibrary;
 import com.lucasterra.gamebattle.integration.steam.response.GetOwnedGamesResponse;
+import com.lucasterra.gamebattle.repositories.GameRepository;
+import com.lucasterra.gamebattle.repositories.UserLibraryRepository;
+import com.lucasterra.gamebattle.repositories.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
+import java.util.Optional;
+
 @Component
 public class SteamSmokeTest implements CommandLineRunner {
 
     private final RestClient steamRestClient;
+    private final UserRepository userRepository;
+    private final GameRepository gameRepository;
+    private final UserLibraryRepository userLibraryRepository;
 
     @Value("${steam.api-key}")
     private String apiKey;
 
-    // Coloque seu ID aqui ou injete via @Value para testar
     @Value("${steam.steamid}")
     private String mySteamId;
 
-    public SteamSmokeTest(RestClient steamRestClient) {
+    public SteamSmokeTest(RestClient steamRestClient, UserRepository userRepository, GameRepository gameRepository, UserLibraryRepository userLibraryRepository) {
         this.steamRestClient = steamRestClient;
+        this.userRepository = userRepository;
+        this.gameRepository = gameRepository;
+        this.userLibraryRepository = userLibraryRepository;
     }
 
     @Override
+    @Transactional
     public void run(String... args) {
         System.out.println("INICIANDO TESTE DA STEAM API...");
 
         try {
             // Endpoint: IPlayerService/GetOwnedGames
-            var result = steamRestClient.get()
+            var response = steamRestClient.get()
                     .uri(uriBuilder -> uriBuilder
                             .path("/IPlayerService/GetOwnedGames/v0001/")
                             .queryParam("key", apiKey)
@@ -39,18 +54,52 @@ public class SteamSmokeTest implements CommandLineRunner {
                     .retrieve()
                     .body(GetOwnedGamesResponse.class);
 
-            if (result != null && result.response() != null) {
-                System.out.println("✅ SUCESSO! Jogos encontrados: " + result.response().gameCount());
-                System.out.println("🎮 Exemplo de jogo: " + result.response().games().get(0).name());
-                var resultado = result.getGames();
-                for (GetOwnedGamesResponse.GameDto game : resultado) {
-                    System.out.println(game.name() + ": " + (game.playtimeForever()/60) + " hours");
-                }
-            } else {
-                System.out.println("⚠️ Resposta vazia da Steam.");
+            if (response == null || response.getGames().isEmpty()) {
+                System.out.println("Nenhum jogo encontrado para o usuário Steam ID: " + mySteamId);
+                return;
             }
+
+            User user = userRepository.findBySteamId(mySteamId)
+                    .orElseGet(() -> userRepository.save(User.builder()
+                            .steamId(mySteamId)
+                            .username("lt") // Placeholder (depois pegamos da API de perfil)
+                            .avatarUrl("TODO")
+                            .build()));
+
+            System.out.println("Usuário processado: " + user.getUsername());
+
+            int novosJogos = 0;
+            for (var gameDto : response.getGames()) {
+                Game game = gameRepository.findByAppId(gameDto.appId())
+                        .orElseGet(() -> {
+                            return gameRepository.save(Game.builder()
+                                    .appId(gameDto.appId())
+                                    .name(gameDto.name())
+                                    .imageUrl("http://media.steampowered.com/steamcommunity/public/images/apps/" + gameDto.appId() + "/Header.jpg")
+                                    .build());
+                        });
+
+                Optional<UserLibrary> libraryEntry = userLibraryRepository.findByUserAndGame(user, game);
+
+                if(libraryEntry.isPresent()) {
+                    UserLibrary lib = libraryEntry.get();
+                    lib.setTotalPlaytime(gameDto.playtimeForever());
+                    userLibraryRepository.save(lib);
+                }
+                else {
+                    userLibraryRepository.save(UserLibrary.builder()
+                            .user(user)
+                            .game(game)
+                            .totalPlaytime(gameDto.playtimeForever())
+                            .build());
+                    novosJogos++;
+                }
+            }
+
+            System.out.println("FINALIZADO! " + response.getGames().size() + " jogos processados.");
+            System.out.println("Jogos novos cadastrados nesta execucao: " + novosJogos);
         } catch (Exception e) {
-            System.out.println("❌ FALHA NO TESTE: " + e.getMessage());
+            System.out.println("FALHA NO TESTE: " + e.getMessage());
             e.printStackTrace();
         }
     }
