ab05c24df2dfb07ceef8d6c2a8e90d41db6f3ec1
Lucas Terra <lucas.terra8587@gmail.com>
2026-02-08 18:50:08 -0300

    feat: add filter for unplayable games

--- src/main/java/com/lucasterra/gamebattle/service/GameService.java
+++ src/main/java/com/lucasterra/gamebattle/service/GameService.java
@@ -14,18 +14,48 @@ public class GameService {
 
     private final UserLibraryRepository userLibraryRepository;
 
+    // Lista de palavras que indicam que o "jogo" não é jogável ou relevante
+    private static final List<String> INVALID_KEYWORDS = List.of(
+            "beta",
+            "test",
+            "demo",
+            "public test",
+            "test server",
+            "trial",
+            "server",
+            "retired",
+            "soundtrack",
+            "dlc",
+            "dedicated server",
+            "playtest",
+            "alpha",
+            "experimental",
+            "testing grounds"
+    );
+
     public List<Game> getLowPlaytimeGames(String steamId) {
         return userLibraryRepository.findByUser_SteamId(steamId).stream()
                 .filter(lib -> lib.getTotalPlaytime() < 180)
+                .filter(lib -> isValidGame(lib.getGame().getName()))
                 .map(lib -> lib.getGame())
                 .toList();
     }
 
     public List<Game> getMostPlayedGames(String steamId) {
         return userLibraryRepository.findByUser_SteamId(steamId).stream()
+                .filter(lib -> isValidGame(lib.getGame().getName()))
                 .sorted(Comparator.comparingInt(lib -> -lib.getTotalPlaytime()))
                 .limit(20)
                 .map(lib -> lib.getGame())
                 .toList();
+    }
+
+    private Boolean isValidGame(String gameName) {
+        if(gameName == null) {
+            return false;
+        }
+
+        String lowerCaseName = gameName.toLowerCase();
+        return INVALID_KEYWORDS.stream().noneMatch(lowerCaseName::contains);
     }
 }
